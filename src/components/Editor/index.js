0518d14ed80501ed93ef0538ed71bd0286ee1253
netsov <a.netsov@gmail.com>
2017-12-30 13:40:49 -0500

    new form in progress

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './style.css';
 
-import { InputField } from '../material/InputField';
+// import { InputField } from '../material/InputField';
 
-import { Modal, Select } from 'antd';
+import { Modal, Select, Form, InputNumber } from 'antd';
 
 import { COIN_PRICE, TRADING_PAIRS, callApi } from '../../utils';
 
-export class Editor extends Component {
+const FormItem = Form.Item;
+
+class _Editor extends Component {
   state = {
     toSymbols: [],
     position: null,
@@ -80,10 +82,10 @@ export class Editor extends Component {
     const {
       __id,
       symbol,
-      quantity,
-      currency,
-      tradeDate,
-      tradePrice,
+      // quantity,
+      // currency,
+      // tradeDate,
+      // tradePrice,
     } = this.state.position;
     return (
       <form>
@@ -108,58 +110,137 @@ export class Editor extends Component {
             ))}
           </Select>
         </fieldset>
+
         <fieldset>
-          <InputField
-            required={true}
-            type="number"
-            step={0.00000001}
+          <InputNumber
             min={0}
-            initialValue={quantity}
-            handleChange={this.handleChange('quantity')}
-            name="Quantity"
-            placeholder="e.g. 0.001"
-            rtl={false}
+            step={0.000001}
+            onChange={this.handleChange('quantity')}
           />
         </fieldset>
-        <fieldset>
-          <datalist id="toSymbols">
-            {this.state.toSymbols.map(toSymbol => (
-              <option key={toSymbol} value={toSymbol}>
-                {toSymbol}
-              </option>
-            ))}
-          </datalist>
-          <InputField
-            list="toSymbols"
-            required={true}
-            initialValue={currency}
-            handleChange={this.handleChange('currency')}
-            name="Currency"
-            placeholder="e.g. USD, BTC"
-          />
-        </fieldset>
-        <fieldset>
-          <InputField
-            required={true}
-            type="number"
-            step={0.00000001}
-            min={0}
-            name="Trade Price"
-            initialValue={tradePrice}
-            handleChange={this.handleChange('tradePrice')}
-            placeholder="Trade Price"
-            rtl={false}
-          />
-        </fieldset>
-        <fieldset>
-          <InputField
-            type="date"
-            initialValue={tradeDate}
-            handleChange={this.handleChange('tradeDate')}
-            name="Trade Date"
-          />
-        </fieldset>
+        {/*<fieldset>*/}
+        {/*<InputField*/}
+        {/*required={true}*/}
+        {/*type="number"*/}
+        {/*step={0.00000001}*/}
+        {/*min={0}*/}
+        {/*initialValue={quantity}*/}
+        {/*handleChange={this.handleChange('quantity')}*/}
+        {/*name="Quantity"*/}
+        {/*placeholder="e.g. 0.001"*/}
+        {/*rtl={false}*/}
+        {/*/>*/}
+        {/*</fieldset>*/}
+        {/*<fieldset>*/}
+        {/*<datalist id="toSymbols">*/}
+        {/*{this.state.toSymbols.map(toSymbol => (*/}
+        {/*<option key={toSymbol} value={toSymbol}>*/}
+        {/*{toSymbol}*/}
+        {/*</option>*/}
+        {/*))}*/}
+        {/*</datalist>*/}
+        {/*<InputField*/}
+        {/*list="toSymbols"*/}
+        {/*required={true}*/}
+        {/*initialValue={currency}*/}
+        {/*handleChange={this.handleChange('currency')}*/}
+        {/*name="Currency"*/}
+        {/*placeholder="e.g. USD, BTC"*/}
+        {/*/>*/}
+        {/*</fieldset>*/}
+        {/*<fieldset>*/}
+        {/*<InputField*/}
+        {/*required={true}*/}
+        {/*type="number"*/}
+        {/*step={0.00000001}*/}
+        {/*min={0}*/}
+        {/*name="Trade Price"*/}
+        {/*initialValue={tradePrice}*/}
+        {/*handleChange={this.handleChange('tradePrice')}*/}
+        {/*placeholder="Trade Price"*/}
+        {/*rtl={false}*/}
+        {/*/>*/}
+        {/*</fieldset>*/}
+        {/*<fieldset>*/}
+        {/*<InputField*/}
+        {/*type="date"*/}
+        {/*initialValue={tradeDate}*/}
+        {/*handleChange={this.handleChange('tradeDate')}*/}
+        {/*name="Trade Date"*/}
+        {/*/>*/}
+        {/*</fieldset>*/}
       </form>
+    );
+  };
+
+  renderForm2 = () => {
+    const { coins } = this.props;
+    const {
+      __id,
+      symbol,
+      quantity,
+      currency,
+      tradeDate,
+      tradePrice,
+    } = this.state.position;
+
+    const { getFieldDecorator } = this.props.form;
+
+    const formItemLayout = {
+      labelCol: { span: 4 },
+      wrapperCol: { span: 18 },
+    };
+
+    return (
+      <Form onSubmit={() => {}}>
+        <FormItem {...formItemLayout} label={'Name'}>
+          {getFieldDecorator('Name', {
+            rules: [
+              {
+                required: true,
+              },
+            ],
+            initialValue: symbol || undefined,
+          })(
+            <Select
+              showSearch
+              disabled={!!__id}
+              placeholder="Select a coin"
+              optionFilterProp="children"
+              onChange={this.handleChange('symbol')}
+              filterOption={(input, option) =>
+                option.props.children
+                  .toLowerCase()
+                  .indexOf(input.toLowerCase()) >= 0
+              }
+              // defaultValue={symbol || undefined}
+            >
+              {coins.map(coin => (
+                <Select.Option key={coin.Name} value={coin.Name}>
+                  {coin.FullName}
+                </Select.Option>
+              ))}
+            </Select>
+          )}
+        </FormItem>
+
+        <FormItem {...formItemLayout} label={'Quantity'}>
+          {getFieldDecorator('Quantity', {
+            rules: [
+              {
+                required: true,
+              },
+            ],
+            initialValue: quantity,
+          })(
+            <InputNumber
+              min={0}
+              step={0.000001}
+              onChange={this.handleChange('quantity')}
+            />
+          )}
+        </FormItem>
+      </Form>
     );
   };
 
@@ -173,8 +254,10 @@ export class Editor extends Component {
         okText="Save"
         onCancel={this.props.closeEditor}
       >
-        {this.renderForm()}
+        {this.renderForm2()}
       </Modal>
     );
   }
 }
+
+export const Editor = Form.create()(_Editor);
